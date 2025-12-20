36702fe4a62c616130710401c87a9ea01d324c5a
Smrita Pandey <spande49@asu.edu>
2025-12-20 15:52:45 +0530

    Manual sync from desktop

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,12 +1,37 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/[root-of-the-server]__4f1bcde5._.js",
+      "static/chunks/1cfed_react-dom_a9cbdca5._.js",
+      "static/chunks/1cfed_83af2a6e._.js",
+      "static/chunks/[root-of-the-server]__d8ffccf9._.js",
+      "static/chunks/Projects_OCR-admission-forms_pages__app_5771e187._.js",
+      "static/chunks/Projects_OCR-admission-forms_pages__app_65757e1f._.js"
+    ],
+    "/_error": [
+      "static/chunks/[root-of-the-server]__fa7c1cc9._.js",
+      "static/chunks/1cfed_react-dom_a9cbdca5._.js",
+      "static/chunks/1cfed_83af2a6e._.js",
+      "static/chunks/[root-of-the-server]__f7999bec._.js",
+      "static/chunks/Projects_OCR-admission-forms_pages__error_5771e187._.js",
+      "static/chunks/Projects_OCR-admission-forms_pages__error_37c7ad90._.js"
+    ]
   },
   "devFiles": [],
   "ampDevFiles": [],
-  "polyfillFiles": [],
+  "polyfillFiles": [
+    "static/chunks/1cfed_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
   "lowPriorityFiles": [],
-  "rootMainFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_199366e1._.js",
+    "static/chunks/1cfed_next_dist_compiled_1ee3f34a._.js",
+    "static/chunks/1cfed_next_dist_client_d7d36a91._.js",
+    "static/chunks/1cfed_next_dist_2481b098._.js",
+    "static/chunks/1cfed_@swc_helpers_cjs_b27e056c._.js",
+    "static/chunks/Projects_OCR-admission-forms_e69f0d32._.js",
+    "static/chunks/Projects_OCR-admission-forms_40b1bc3b._.js"
+  ],
   "ampFirstPages": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
